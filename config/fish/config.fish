880ad13d4907ec0dec89943a6cfb74b8273eaf2a
jackwthake <jackthake@hotmail.com>
2026-09-16 17:22:31 -0700

    Added go and OCaml paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,4 +3,19 @@ if status is-interactive
     fastfetch
     pp ~/Code
 end
-export PATH="$HOME/.local/bin:$PATH"
+
+fish_add_path $HOME/.local/bin
+
+# Go
+set -gx GOPATH $HOME/go
+fish_add_path /usr/local/go/bin
+fish_add_path $GOPATH/bin
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/jwt/.opam/opam-init/init.fish' && source '/home/jwt/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
